587a39c331c78c44e46ec9920aab190353281a79
Daniel Bell <40198194@eeecs.qub.ac.uk>
2020-10-14 15:45:42 +0100

    compositionComparison() normalisedFrequencies(), near complete. Extensible for n genomes and polynucleotides. Descriptions in comments

--- Nucleotides.py
+++ Nucleotides.py
@@ -1,5 +1,7 @@
 # https://en.wikipedia.org/wiki/Tandem_repeat
 import Coronaviridae
+import matplotlib.pyplot as plt
+import numpy as np
 
 nucleotide_bases = ['A', 'C', 'G', 'T']  # Adenine, Cytosine, Guanine, Thymine
 
@@ -28,32 +30,81 @@ def basesCombinationRecursive(bases, string, n, str_length, combinations):
     return combinations
 
 
-dinucleotide = basesCombination(nucleotide_bases, 2)
-print("Dinucleotide: " + str(dinucleotide))
+dimers = basesCombination(nucleotide_bases, 2)  # dinucleotide
+print("Dimers: " + str(dimers))
 
-trinucleotide = basesCombination(nucleotide_bases, 3)
-print("Trinucleotide: " + str(trinucleotide))
+trimers = basesCombination(nucleotide_bases, 3)  # trinucleotides
+print("Trimers: " + str(trimers))
 
-tetranucledotide = basesCombination(nucleotide_bases, 4)
-print("Tetranucledotide: " + str(tetranucledotide))
-
+tetramers = basesCombination(nucleotide_bases, 4)  # tetranucledotides
+print("Tetramers: " + str(tetramers))
 
 coronaviridae = Coronaviridae.readInGenomes()
 
-def nucleotideComposition(genome_dict):
-    dict = {'A': 'Adenine', 'C': 'Cytosine', 'G': 'Guanine', 'T': 'Thymine'} # Print name
 
-    print("-- " + genome_dict['coronavirus'] + " --") # coronavirus
-    print(genome_dict['description']) # description
+def nucleotideComposition(genome_dict):
+    dict = {'A': 'Adenine', 'C': 'Cytosine', 'G': 'Guanine', 'T': 'Thymine'}  # Print name
+
+    print("-- " + genome_dict['coronavirus'] + " --")  # coronavirus
+    print(genome_dict['description'])  # description
     sequence = genome_dict['sequence']
-    print(sequence) # sequence
+    print(sequence)  # sequence
 
     for n in nucleotide_bases:
         n_count = sequence.count(n)
         n_comp = round(n_count / len(sequence) * 100, 2)
         print(dict[n] + " Composition: " + str(n_comp) + "%")
 
+
 MERS = nucleotideComposition(coronaviridae[0])
 SARS = nucleotideComposition(coronaviridae[0])
 SARSCoV2 = nucleotideComposition(coronaviridae[0])
 
+
+def compositionComparison(polymers, *genomes):
+    # Plots a chart -  Normalised polynucleotide frequences of bases composition, for n genomes of interest
+
+    # polymers - holds basesCombination() output array, x axis
+    # *genomes - array holds genome dictionaries, to be plotted
+
+    # To display relevant titles
+    n = len(polymers[0])  # no. nucleotide units in polymer
+    polynucleotide = {1: "Nucleotide", 2: 'Dinucleotide', 3: 'Trinucleotide', 4: 'Tetranucleotide', 5: 'Pentanucleotide'}
+    n_coronavirus = "Coronaviridae" if len(*genomes) > 1 else "Coronavirus"  # plural or singular
+
+    # Plot onwards
+    plt.figure(figsize=(50, 25))
+
+    # X axis
+    plt.xticks(rotation=60)
+    plt.xlabel(polynucleotide[n], fontsize=30)
+    plt.title(polynucleotide[n] + " Composition of " + n_coronavirus)
+
+    # Y axis
+    plt.ylabel("Normalised " + polynucleotide[n] + " Frequency", fontsize=30)
+    plt.ylim(0, 0.10)
+    plt.yticks(np.arange(0, 0.10, 0.01))
+
+    # Content (n genomes)
+    for g in genomes:
+        plt.plot(polymers, normalisedFrequencies(polymers, genomes[g]), linewidth=5, color="red")
+        plt.legend(genomes['coronavirus'])
+
+    # Additions
+    plt.grid(True)
+    plt.show()
+    plt.draw()
+
+    # plt.savefig('polymer_composition.png', format="png")
+
+
+def normalisedFrequencies(polymers, genome_sequence):
+    # Calculates no. appearances a polymer subsequence appears in complete genome sequence, as a percentage
+    # returns array of percentages, for each polymer, to be plotted in compositionComparison()
+    normalisedfreq = []
+    for p in polymers:
+        count = genome_sequence.count(p)
+        nf = count / len(genome_sequence)
+        normalisedfreq.append(nf)
+
+    return normalisedfreq
