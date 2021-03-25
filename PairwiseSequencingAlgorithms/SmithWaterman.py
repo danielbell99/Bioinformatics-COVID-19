a62d220ec3f55d1fb038a5bbacceeb0288d653ba
40198194 <dbell30@qub.ac.uk>
2021-03-25 10:07:02 +0000

    PSA's Code Quality: comments, readability, extensible, reuse, continuty

--- PairwiseSequencingAlgorithms/SmithWaterman.py
+++ PairwiseSequencingAlgorithms/SmithWaterman.py
@@ -3,8 +3,7 @@ import StandardFunctions as sf
 
 
 def SmithWaterman(bio_type, *args, **kwargs):
-    """
-    Local sequence alignment algorithm
+    """Local sequence alignment algorithm.
 
     :param bio_type: "dna" or "protein"
     :param tuple *args: pair of genomes' str names
@@ -15,9 +14,10 @@ def SmithWaterman(bio_type, *args, **kwargs):
 
     m, n = len(seqA), len(seqB)  # let m, n denote the lengths of two sequences
 
-    # Dynamic Programming Table - Path traceback pointer matrix
-    score_matrix = tasks.zeros((m+1, n+1))
-    pointer = tasks.zeros((m+1, n+1))  # holds traceback path (same dimensions)
+    # Dynamic Programming Table - score matrix
+    score_matrix = tasks.empty_matrix(m+1, n+1)  # '+1' - far-left col & top row represent indexes of sequences' chars
+    # Path Traceback - pointer matrix
+    pointer_matrix = tasks.empty_matrix(m+1, n+1)  # stores traceback path (same dimensions)
 
     max_score = 0  # Initial maximum score obtainable
     # Contents & Declare Pointers
@@ -28,41 +28,41 @@ def SmithWaterman(bio_type, *args, **kwargs):
             above_left_score = score_matrix[i-1][j-1] + tasks.match(seqA[i-1], seqB[j-1], points_scheme)  # Assign points to position
             score_matrix[i][j] = max(0, above_score, left_score, above_left_score)  # cannot be a negative number
 
+            # Update w/ Optimal Traceback Path
             if score_matrix[i][j] == above_score:
-                pointer[i][j] = 2  # 2 - left
+                pointer_matrix[i][j] = 1  # 1 - left
             if score_matrix[i][j] == left_score:
-                pointer[i][j] = 1  # 1 - above
+                pointer_matrix[i][j] = 2  # 2 - above
             if score_matrix[i][j] == above_left_score:
-                pointer[i][j] = 3  # 3 - above_left
+                pointer_matrix[i][j] = 3  # 3 - above_left
             if score_matrix[i][j] >= max_score:
                 max_i, max_j = i, j
                 max_score = score_matrix[max_i][max_j];
             if score_matrix[i][j] == 0:  # 0 - End of path
-                pointer[i][j] = 0
+                pointer_matrix[i][j] = 0
 
-    align1, align2 = '', ''  # initial sequences
     i, j = max_i, max_j  # indices of path starting point
-
-    # traceback, follow pointers
-    while pointer[i][j] != 0:
-        if pointer[i][j] == 3:
-            i -= 1
-            j -= 1
-            align1 += seqA[i]
-            align2 += seqB[j]
-        elif pointer[i][j] == 2:
-            j -= 1
-            align1 += '-'
-            align2 += seqB[j]
-        elif pointer[i][j] == 1:
+    align1, align2 = '', ''  # initial sequences
+    # Traceback - follows pointers
+    while pointer_matrix[i][j] != 0:  # 0 - End of path
+        if pointer_matrix[i][j] == 1:  # 1 - left
             i -= 1
             align1 += seqA[i]
             align2 += '-'
+        elif pointer_matrix[i][j] == 2:  # 2 - above
+            j -= 1
+            align1 += '-'
+            align2 += seqB[j]
+        elif pointer_matrix[i][j] == 3:  # 3 - above_left
+            i -= 1
+            j -= 1
+            align1 += seqA[i]
+            align2 += seqB[j]
 
 
     # Score & Alignment
-    align1, symbols, align2, str_score = tasks.score_alignment(align1, align2, points_scheme)
-    alignments = [align1, symbols, align2, str_score]
+    align1, signs, align2, str_score = tasks.score_alignment(align1, align2, points_scheme)
+    alignments = [align1, signs, align2, str_score]
 
     # Sequence Identity
     seq_identity = tasks.sequence_identity(align1, align2)  # tuple (seq_id, gapless_seq_id)
--- PairwiseSequencingAlgorithms/tasks.py
+++ PairwiseSequencingAlgorithms/tasks.py
@@ -14,7 +14,7 @@ def save(algorithm_name, bio_type, output_name, alignments, seq_identity):
     :param str bio_type: "dna" or "protein"
     :param str output_name: segment of filename that lists virus names (e.g. "_MERS-MT387202_SARS-CoV-JQ316196")
     :param Alignment alignments: stores all alignments btwn. a pair of sequences and match score
-    :param tuple seq_identity: [0] | 'seq_id' (match % w/ gaps) ; [1] | 'gapless_seq_id' (match % w/out gaps)
+    :param tuple seq_identity: [0] = 'seq_id' (match % w/ gaps) ; [1] = 'gapless_seq_id' (match % w/out gaps)
     """
     # File
     path_filename = os.path.join('data/Pairwise Sequencing/' + algorithm_name + '/', bio_type.lower() + output_name + '.txt')  # Defines path and filename
@@ -58,92 +58,96 @@ def sequence_identity(align1, align2):
     gapless_seq_id = (100 * sum(matches)) / gapless_length  # raw %
 
     print("gapless_length", gapless_length)
-    print("gapless_seq_id", str(round(gapless_seq_id, 2)) + "%")
+    print("gapless_seq_id", str(round(gapless_seq_id, 2)) + '%')
 
     return (seq_id, gapless_seq_id)
 
 
-def zeros(shape):
+def empty_matrix(x, y):
+    """Creates empty matrix of determined shape for Dynamic Programming Table or Pointer Matrix.
+
+    :param x: row length (m+1)
+    :param y: column length (n+1)
+    :return: empty matrix of defined size (0 values)
     """
+    matrix = []
+    for y_ in range(y):
+        matrix.append([])
+        for x_ in range(x):
+            matrix[-1].append(0)  # 0 values
 
-    :param shape:
-    :return:
-    """
-    retval = []
-    for x in range(shape[0]):
-        retval.append([])
-        for y in range(shape[1]):
-            retval[-1].append(0)
-    return retval
+    return matrix
 
 
-def match(x, y, points_scheme):
-    """ Determine type of match between aligned sequences' characters
+def match(seqA_c, seqB_c, points_scheme):
+    """Determine type of match between aligned sequences' characters.
 
-    :param x: seqA char
-    :param y: seqB char
+    :param seqA_c: seqA char
+    :param seqB_c: seqB char
     :param list point_scheme: deterministic list of points to score (previously defined)
     :return: float: calculated points
     """
-    if x == y: return points_scheme[0]  # 'match_points'
-    elif x != y: return points_scheme[1]  # 'mismatch_points'
-    elif x == '-' or y == '-': return points_scheme[2]  # 'gap_points'
-    else: return 0  # Exception Handling
+    if seqA_c == seqB_c:
+        return points_scheme[0]  # 'match_points'
+    elif seqA_c != seqB_c:
+        return points_scheme[1]  # 'mismatch_points'
+    elif seqA_c == '-' or seqB_c == '-':
+        return points_scheme[2]  # 'gap_points'
+    else:
+        return 0  # Exception Handling
 
 
-def gap_function(x, y):
+def gap_function(idx, len):  # mandatory pass of 'idx' by 'pairwise2.align's 'x'
     """Deducts points for gaps when matching up sequencing, using a logarithmic scale.
 
-    :param int x: index to start of gap
-    :param int y: gap length
+    :param int idx: index to start of gap
+    :param int len: gap length
     :return: log(): producing negative decimal number
     """
-    if y == 0:  # Asks if there's a gap
+    if len == 0:  # Asks if there's a gap
         return 0
-    elif y == 1:  # Gap
+    elif len == 1:  # Gap
         return -1
     # Consecutive gaps
-    return round(-((log(y) / 2) + (2 + (y / 4))), 2)  # y = 1 ; return -2.25
+    return round(-((log(len) / 2) + (2 + (len / 4))), 2)  # len = 1 ; return -2.25
 
 
 def score_alignment(align1, align2, points_scheme):
-    """Mimicking Biopython's 'format_alignment'
+    """Return format of Alignment Algorithm for output files.
 
     :param str align1: optimally aligned 'seqA' for 'seqB''s 'align2'
     :param str align2:  optimally aligned 'seqB' for 'seqA''s 'align1'
     :param list point_scheme: deterministic list of points to score (previously defined)
     :return: str align1, symbols, align2, score: cleansed output vars of alignment algorithm
     """
+    score = 0
+    signs = ""
+
     # Reset values
+    i, j = 0, 0
     align1 = align1[::-1]  # reverse sequences
     align2 = align2[::-1]
-    i, j = 0, 0
-    # calcuate identity, score and aligned sequeces
-    symbols = ""
-    found, score, identity = 0, 0, 0
 
     for i in range(0, len(align1)):
         if align1[i] == align2[i]:  # match
-            symbols += '|'
-            identity += 1
+            signs += '|'
             score += match(align1[i], align2[i], points_scheme)
         elif align1[i] != align2[i] and align1[i] != '-' and align2[i] != '-':  # mismatch
             score += match(align1[i], align2[i], points_scheme)
-            symbols += '.'
-            found = 0  # False
+            signs += '.'
         elif align1[i] == '-' or align2[i] == '-':  # gap
-            symbols += ' '
+            signs += ' '
             score += points_scheme[2]  # 'gap_points'
 
-    print(align1 + '\n' + symbols + '\n' + align2)
+    print(align1 + '\n' + signs + '\n' + align2)
     score = '  Score=' + str(score)
     print(score)
 
-    return align1, symbols, align2, score
+    return align1, signs, align2, score
 
 
 def preparation(bio_type, *args, **kwargs):
-    """
+    """Loads in datasets as sequences and establishes points scheme for alignment algorithm.
 
     :param bio_type: "dna" or "protein"
     :param str name1: name of first genome
@@ -153,11 +157,9 @@ def preparation(bio_type, *args, **kwargs):
     """
     bio_type = bio_type.lower()
     if bio_type == "dna":
-        directory = 'src/'
         seqA = sf.dna_sequence(str(args[0][0]))  # Target sequence
         seqB = sf.dna_sequence(str(args[0][1]))  # Query sequence
     elif bio_type == "protein":
-        directory = 'data/Syntheses/'
         seqA = sf.protein_sequence(str(args[0][0]))  # Passing names as str
         seqB = sf.protein_sequence(str(args[0][1]))
     else:  # Exception Handling
