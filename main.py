46e256a48c6f11f9da06cb809fbfbf3072ed9eb4
40198194 <dbell30@qub.ac.uk>
2021-04-03 17:17:06 +0100

    Analyses now includes ''

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ import test
 """ Import Genomes """
 # API Parameters
 #db = 'nucleotide'  # 'protein'
-#ids = ['844174433']  # append list of IDs you want to fetch from NCBI
+#ids = ['']  # append list of IDs you want to fetch from NCBI  # 844174433, AP017922
 #genomes = dataimport.read_genomes(db, ids)  # static
 
 
@@ -21,15 +21,15 @@ import test
 #Nucleotides.base_content("SARS-CoV-JQ316196")
 #Nucleotides.base_content("MERS-MT387202")
 #Nucleotides.base_content("SARS-CoV-2-MT873892")
-
+#Nucleotides.base_content("StaphylococcusAureus-AP017922")
 
 #Nucleotides.base_combinations(2)  # Polymer Bases Combinations - 3, 4, 5, 6
 
-#Nucleotides.composition_comparison(2, "SARS-CoV-JQ316196", "MERS-MT387202", "SARS-CoV-2-MT873892")
-#Nucleotides.composition_comparison(3, "SARS-CoV-JQ316196", "MERS-MT387202", "SARS-CoV-2-MT873892")
-#Nucleotides.composition_comparison(4, "SARS-CoV-JQ316196", "MERS-MT387202", "SARS-CoV-2-MT873892")
-#Nucleotides.composition_comparison(5, "SARS-CoV-JQ316196", "MERS-MT387202", "SARS-CoV-2-MT873892")
-#Nucleotides.composition_comparison(6, "SARS-CoV-JQ316196", "MERS-MT387202", "SARS-CoV-2-MT873892")
+#Nucleotides.composition_comparison(2, "SARS-CoV-JQ316196", "MERS-MT387202", "SARS-CoV-2-MT873892", "StaphylococcusAureus-AP017922")
+#Nucleotides.composition_comparison(3, "SARS-CoV-JQ316196", "MERS-MT387202", "SARS-CoV-2-MT873892", "StaphylococcusAureus-AP017922")
+#Nucleotides.composition_comparison(4, "SARS-CoV-JQ316196", "MERS-MT387202", "SARS-CoV-2-MT873892", "StaphylococcusAureus-AP017922")
+#Nucleotides.composition_comparison(5, "SARS-CoV-JQ316196", "MERS-MT387202", "SARS-CoV-2-MT873892", "StaphylococcusAureus-AP017922")
+#Nucleotides.composition_comparison(6, "SARS-CoV-JQ316196", "MERS-MT387202", "SARS-CoV-2-MT873892", "StaphylococcusAureus-AP017922")
 
 
 """ Cluster Analysis """
@@ -44,6 +44,7 @@ import test
 #Syntheses.protein("SARS-CoV-JQ316196")
 #Syntheses.protein("MERS-MT387202")
 #Syntheses.protein("SARS-CoV-2-MT873892")
+Syntheses.protein("StaphylococcusAureus-AP017922")
 
 
 """ Pairwise Sequencing """
@@ -60,8 +61,8 @@ import test
 
 """ Alignment Files """
 # All sequences considered in Alignment files (.aln)
-#Alignment.create("DNA")
-#Alignment.create("Protein")
+Alignment.create("DNA")
+Alignment.create("Protein")
 
 """ Multiple Sequence Alignment """
 """ Bokeh does not support outputs w/ PyCharm (Google for evidence)
@@ -71,7 +72,7 @@ Test run Jupyter Notebook version
 """
 
 """ Testing """
-test.run()
+#test.run()
 
 
 print("END OF MAIN")
